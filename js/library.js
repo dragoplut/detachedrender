2ad5ad9c7e0609eebbfea5c1ab7296fe485d09ce
dragoplut <oleksandr_p@i.ua>
2016-01-26 13:00:30 +0200

    changed code, moving still don't work

--- js/library.js
+++ js/library.js
@@ -1,18 +1,10 @@
-/**
- * Created by oleksandr on 23.01.16.
- */
 
-/**
- * Render input param's
- * render 'arrow' & 'close' buttons
- * result: append rendered HTML obj to parent obj, destination: option.$parent
- * @param option
- */
+var elements = [];
+
 function render(option) {
     // render block and insert it to parent block
     if (option.$parent === undefined || option.top === undefined || option.left === undefined){
-        alert('Відсутній обовязковий параметр! Виконання припинено.');
-        return;
+        throw new Error('Necessarilly parameter absent!');
     }
     if (option.backgroundColor === undefined){
         option.backgroundColor = '#FFFFF0';
@@ -43,15 +35,63 @@ function render(option) {
     $elem[0].style.height = option.height + 'px';
     $elem[0].style.width = option.width + 'px';
 
-    var arrowsBlock = '<div class="btn-group" style="left: 5px; top: 55px"><button id="leftAr' + option.ID + '" class="btn btn-sm btn-default"><i class="fa fa-arrow-left"></i></button><button id="rightAr' + option.ID + '" class="btn btn-sm btn-default"><i class="fa fa-arrow-right"></i></button><button id="upAr' + option.ID + '" class="btn btn-sm btn-default"><i class="fa fa-arrow-up"></i></button><button id="downAr' + option.ID + '" class="btn btn-sm btn-default"><i class="fa fa-arrow-down"></i></button></div>';
+    var arrowsBlock = '<div class="btn-group" style="left: 5px; top: 55px"><button data-id="' + option.ID + '" class="btn-left btn btn-sm btn-default"><i class="fa fa-arrow-left"></i></button><button data-id="' + option.ID + '" class="btn-right btn btn-sm btn-default"><i class="fa fa-arrow-right"></i></button><button data-id="' + option.ID + '" class="btn-up btn btn-sm btn-default"><i class="fa fa-arrow-up"></i></button><button data-id="' + option.ID + '" class="btn-bottom btn btn-sm btn-default"><i class="fa fa-arrow-down"></i></button></div>';
     var titleBlock = '<div class="panel-heading" style="text-align: center"><h7 class="panel-title">' + option.title + '</h7></div>';
-    var closeBtn = '<div class="btn-group" style="float: right"><button id="closeBtn' + option.ID + '" class="btn btn-xs btn-default"><i class="fa fa-close"></i></button></div>';
+    var closeBtn = '<div class="btn-group" style="float: right"><button data-id="' + option.ID + '" class="btn-remove btn btn-xs btn-default"><i class="fa fa-close"></i></button></div>';
     $elem.append(closeBtn, titleBlock, arrowsBlock);
 
-    console.log($elem);
+    console.log($elem, elements[option.ID]);
+    elements[option.ID] = $elem;
 
     option.$parent.append($elem);
 }
+
+
+function left(id) {
+    console.info('left', id);
+
+    var $elem = elements[id];
+    var styleLeft = parseInt($elem.style.left) - 10;
+    $elem.style.left = styleLeft + 'px';
+}
+
+function right(id) {
+    console.info('right', id);
+
+    var $elem = elements[id];
+    var styleLeft = parseInt($elem.style.left) - 10;
+    $elem.style.left = styleLeft + 'px';
+}
+
+//Changed fom 'top()' to 'up()' because of error 'top' attribute in 'render'
+function up(id) {
+    console.info('up', id);
+
+    var $elem = elements[id];
+    var styleLeft = parseInt($elem.style.left) - 10;
+    $elem.style.left = styleLeft + 'px';
+}
+
+function bottom(id) {
+    console.info('bottom', id);
+
+    var $elem = elements[id];
+    var styleLeft = parseInt($elem.style.left) - 10;
+    $elem.style.left = styleLeft + 'px';
+}
+
+/**
+ * Removing is not working correct after removing one element
+ * Difference between obj ID in arr & obj id in HTML lead to removing wrong object!
+  * @param id
+ */
+function remove(id) {
+    console.info('remove', id);
+    elements[id].remove();
+    elements.splice(id, 1);
+    console.info(elements, ' after remove');
+}
+
 
 /**
  * Track's click on any point of <div id="#mainDiv"></div>
@@ -62,26 +102,58 @@ function render(option) {
  * Remove html element of 'id' holder
  */
 $(document).ready(function(){
-    $('#mainDiv').click(function(){
-        console.log(event.target.id);
-        var targetId = event.target.id;
-        var posit = parseInt(targetId);
-        var ID = targetId.slice(targetId.indexOf(posit));
-        var $elem = document.getElementById(ID);
-        if (targetId.indexOf('leftAr') > -1){
-            var styleLeft = parseInt($elem.style.left) - 10;
-            $elem.style.left = styleLeft + 'px';
-        } else if (targetId.indexOf('rightAr') > -1){
-            var styleRight = parseInt($elem.style.left) + 10;
-            $elem.style.left = styleRight + 'px';
-        } else if (targetId.indexOf('upAr') > -1){
-            var styleUp = parseInt($elem.style.top) - 10;
-            $elem.style.top = styleUp + 'px';
-        } else if (targetId.indexOf('downAr') > -1){
-            var styleDown = parseInt($elem.style.top) + 10;
-            $elem.style.top = styleDown + 'px';
-        } else if (targetId.indexOf('closeBtn') > -1){
-            $elem.remove();
-        }
+
+    $('.btn-left').on('click', function(){
+        console.info('btn-left click');
+        var id = this.dataset.id;
+        left(id);
     });
+
+    $('.btn-right').on('click', function(){
+        console.info('btn-right click');
+        var id = this.dataset.id;
+        right(id);
+    });
+
+    $('.btn-top').on('click', function(){
+        console.info('btn-up click');
+        var id = this.dataset.id;
+        up(id);
+    });
+
+    $('.btn-buttom').on('click', function(){
+        console.info('btn-bottom click');
+        var id = this.dataset.id;
+        bottom(id);
+    });
+
+    $('.btn-remove').on('click', function(){
+        console.info('btn-remove click');
+        var id = this.dataset.id;
+        remove(id);
+    });
+
+
+//     $('#mainDiv').click(function(){
+//         console.log(event.target.id);
+//         var targetId = event.target.id;
+//         var posit = parseInt(targetId);
+//         var ID = targetId.slice(targetId.indexOf(posit));
+//         var $elem = document.getElementById(ID);
+//         if (targetId.indexOf('leftAr') > -1){
+//             var styleLeft = parseInt($elem.style.left) - 10;
+//             $elem.style.left = styleLeft + 'px';
+//         } else if (targetId.indexOf('rightAr') > -1){
+//             var styleRight = parseInt($elem.style.left) + 10;
+//             $elem.style.left = styleRight + 'px';
+//         } else if (targetId.indexOf('upAr') > -1){
+//             var styleUp = parseInt($elem.style.top) - 10;
+//             $elem.style.top = styleUp + 'px';
+//         } else if (targetId.indexOf('downAr') > -1){
+//             var styleDown = parseInt($elem.style.top) + 10;
+//             $elem.style.top = styleDown + 'px';
+//         } else if (targetId.indexOf('closeBtn') > -1){
+//             $elem.remove();
+//         }
+//     });
 });
